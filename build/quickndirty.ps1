7493015bd0bc09826d7e52348435eb120e6f8f85
Jeroen Koopman <jkoopman@asp4all.nl>
2017-07-11 13:25:51 +0200

    New version

--- build/quickndirty.ps1
+++ build/quickndirty.ps1
@@ -6,10 +6,10 @@ Write-Verbose -Message "$env:COMPUTERNAME : dotsourcing the cBuildManifest" -Ver
 Write-Verbose -Message "$env:COMPUTERNAME : git commit -am 'New Version'" -Verbose
 git commit -am 'New version'
 
-$remote = git remote
-Write-Verbose -Message "$env:COMPUTERNAME : git remote : $remote" -Verbose
+$remote = git remote 
+Write-Verbose -Message "$env:COMPUTERNAME : git remote(s) : $remote" -Verbose
 
 $remote.ForEach({
-    Write-Verbose -Message "$env:COMPUTERNAME : git push $remote master" -Verbose
+    Write-Verbose -Message "$env:COMPUTERNAME : git push $_ master" -Verbose
     git push $_ master
 })
